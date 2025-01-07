e77a31b44b528afc50e71ae9be3da940e8e94ff9
Tom Hennessy <tomlhennessy@outlook.com>
2025-01-07 10:38:26 +1000

    dashboard

--- components/Login.js
+++ components/Login.js
@@ -14,12 +14,13 @@ export default function Login() {
 
       <p>You&apos;re one step away!</p>
 
-      <input className='max-w-[400px] w-full mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400' />
-      <input className='max-w-[400px] w-full mx-auto px-4 py-2 sm:py-3 border border-solid border-indigo-400' />
+      <input className='max-w-[400px] w-full mx-auto px-3 py-2 sm:py-3 border border-solid border-indigo-400 duration-200 hover:border-indigo-600 focus:border-indigo-600 rounded-full outline-none' placeholder='email' />
+      <input className='max-w-[400px] w-full mx-auto px-3 py-2 sm:py-3 border border-solid border-indigo-400 duration-200 hover:border-indigo-600 focus:border-indigo-600 rounded-full outline-none' placeholder='password' type='password' />
 
       <div className='max-w-[400px] w-full mx-auto'>
         <Button text='submit' full />
       </div>
+      <p className='text-center'>Don&apos;t have an account? <span className='text-indigo-600'>Sign up</span></p>
     </div>
   )
 }
