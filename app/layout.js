69713dcbe5fbb6e186d1134627785701834ddf26
Tom Hennessy <tomlhennessy@outlook.com>
2025-01-08 15:41:47 +1000

    auth context

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import './globals.css';
 import { Fugaz_One, Open_Sans } from "next/font/google";
+import { AuthProvider } from '@/context/AuthContext';
 
 const opensans = Open_Sans({
   subsets: ["latin"],
@@ -37,13 +38,15 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body
-        className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + opensans.className}
-      >
-        {header}
-        {children}
-        {footer}
-      </body>
+      <AuthProvider>
+        <body
+          className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + opensans.className}
+        >
+          {header}
+          {children}
+          {footer}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
